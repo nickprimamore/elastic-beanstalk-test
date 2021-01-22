d1541b91913d6149482e6f2ade5a0c55da9027c5
Nick Primamore <lancernp@gmail.com>
2021-01-22 18:23:35 -0500

    Proxy to port 4001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const PORT = process.env.HTTP_PORT || 8081;
+const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
 
 app.use(cors());
